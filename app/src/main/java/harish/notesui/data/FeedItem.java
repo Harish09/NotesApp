95b1d6467a18e6cdc49831d479431902af84e6d5
Faizaan <aulisius7@gmail.com>
2016-09-06 23:22:44 +0530

    upload activity

--- app/src/main/java/harish/notesui/data/FeedItem.java
+++ app/src/main/java/harish/notesui/data/FeedItem.java
@@ -1,11 +1,59 @@
 package harish.notesui.data;
 
-public class FeedItem {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class FeedItem implements Serializable {
 	private String hashtag;
 	private int id;
 	private String name, status, image, profilePic, timeStamp, url;
 
 	public FeedItem() {
+	}
+
+
+	public static FeedItem fromJSON(JSONObject feedObj) throws JSONException {
+
+		FeedItem item = new FeedItem ();
+
+		item.setId(feedObj.getInt("id"));
+
+		item.setName(feedObj.getString("name"));
+
+		String image = feedObj.isNull("image") ? null : feedObj.getString("image");
+		item.setImage(image);
+
+		item.setStatus(feedObj.getString("status"));
+
+		item.setProfilePic(feedObj.getString("profilePic"));
+
+		item.setTimeStamp(feedObj.getString("timeStamp"));
+
+		String feedUrl = feedObj.isNull("url") ? null : feedObj.getString("url");
+		item.setUrl(feedUrl);
+
+		String tag = feedObj.isNull("hashtag") ? null : feedObj.getString("hashtag");
+		item.setHashtag(tag);
+
+		return item;
+	}
+
+	public JSONObject toJSON() throws JSONException {
+		return new JSONObject ()
+				.put ("id", this.getId ())
+				.put ("name", this.getName ())
+				.put ("image", this.getImage ())
+				.put ("status", this.getStatus ())
+				.put ("profilePic", this.getProfilePic ())
+				.put ("timeStamp", this.getTimeStamp ())
+				.put ("url", this.getUrl ())
+				.put ("hashtag", this.getHashtag ());
 	}
 
 	public String getHashtag() {
@@ -25,8 +73,7 @@ public class FeedItem {
 	}
 
 	public FeedItem(int id, String name, String image, String status,
-			String profilePic, String timeStamp, String url, String hashtag) {
-		super();
+	                String profilePic, String timeStamp, String url, String hashtag) {
 		this.id = id;
 		this.name = name;
 		this.image = image;
